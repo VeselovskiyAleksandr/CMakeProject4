46d88966b717971c9d1cdc29f9b7ba13bb194b71
VeselovskiyAleksandr <79366865+VeselovskiyAleksandr@users.noreply.github.com>
2022-10-29 04:03:34 +0300

    CMakeProject4

--- CMakeProject4/CMakeProject4.cpp
+++ CMakeProject4/CMakeProject4.cpp
@@ -10,53 +10,29 @@ using namespace std;
 int main()
 {
 	setlocale (LC_ALL, "rus");
-	string name, city, command;
+	string  command;
+	cpr::Response r;
 	do {
 		cout << "Введите команду( get, post, put, delete, patch), для выхода из программы введите exit\n";
 		cin >> command;
 		if (command == "get") {
-			cout << "Введите ключ\n";
-			cin >> name;
-			cout << "Введите второй ключ\n";
-			cin >> city;
 			cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
-			cpr::Payload({ {"name", name.c_str()}, { "city", city.c_str() } });
 			cout << r.text;
 		}
 		else if (command == "put") {
-			cout << "Введите ключ\n";
-			cin >> name;
-			cout << "Введите второй ключ\n";
-			cin >> city;
 			cpr::Response r = cpr::Put(cpr::Url("http://httpbin.org/put"));
-			cpr::Payload({ {"name", name.c_str()}, { "city", city.c_str() } });
 			cout << r.text;
 		}
 		else if (command == "patch") {
-			cout << "Введите ключ\n";
-			cin >> name;
-			cout << "Введите второй ключ\n";
-			cin >> city;
 			cpr::Response r = cpr::Patch(cpr::Url("http://httpbin.org/patch"));
-			cpr::Payload({ {"name", name.c_str()}, { "city", city.c_str() } });
 			cout << r.text;
 		}
 		else if (command == "post") {
-			cout << "Введите ключ\n";
-			cin >> name;
-			cout << "Введите второй ключ\n";
-			cin >> city;
 			cpr::Response r = cpr::Post(cpr::Url("http://httpbin.org/post"));
-			cpr::Payload({ {"name", name.c_str()}, { "city", city.c_str() } });
 			cout << r.text;
 		}
 		else if (command == "delete") {
-			cout << "Введите ключ\n";
-			cin >> name;
-			cout << "Введите второй ключ\n";
-			cin >> city;
 			cpr::Response r = cpr::Delete(cpr::Url("http://httpbin.org/delete"));
-			cpr::Payload({ {"name", name.c_str()}, { "city", city.c_str() } });
 			cout << r.text;
 		}
 	} while (command != "exit");

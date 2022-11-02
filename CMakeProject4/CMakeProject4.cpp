55bbc64b05a5f8fb23404e8ed3f62cbcedd4ab72
VeselovskiyAleksandr <79366865+VeselovskiyAleksandr@users.noreply.github.com>
2022-11-02 08:06:01 +0300

    test1 and test2

--- CMakeProject4/CMakeProject4.cpp
+++ CMakeProject4/CMakeProject4.cpp
@@ -1,4 +1,4 @@
-﻿// CMakeProject4.cpp: определяет точку входа для приложения.
+﻿// eProject4.cpp: определяет точку входа для приложения.
 //
 
 #include "CMakeProject4.h"
@@ -10,31 +10,41 @@ using namespace std;
 int main()
 {
 	setlocale (LC_ALL, "rus");
-	string  command;
-	cpr::Response r;
-	do {
-		cout << "Введите команду( get, post, put, delete, patch), для выхода из программы введите exit\n";
-		cin >> command;
-		if (command == "get") {
-			cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
-			cout << r.text;
-		}
-		else if (command == "put") {
-			cpr::Response r = cpr::Put(cpr::Url("http://httpbin.org/put"));
-			cout << r.text;
-		}
-		else if (command == "patch") {
-			cpr::Response r = cpr::Patch(cpr::Url("http://httpbin.org/patch"));
-			cout << r.text;
-		}
-		else if (command == "post") {
-			cpr::Response r = cpr::Post(cpr::Url("http://httpbin.org/post"));
-			cout << r.text;
-		}
-		else if (command == "delete") {
-			cpr::Response r = cpr::Delete(cpr::Url("http://httpbin.org/delete"));
-			cout << r.text;
-		}
-	} while (command != "exit");
+	//int testNumber = 0;
+	//cout << "Введите номер задания\n";
+	//cin >> testNumber;
+	//switch (testNumber) {
+	//case 1:
+
+		string  command;
+		do {
+			cout << "Введите команду( get, post, put, delete, patch), для выхода из программы введите exit\n";
+			cin >> command;
+			if (command == "get") {
+				cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
+				cout << r.text;
+			}
+			else if (command == "put") {
+				cpr::Response r = cpr::Put(cpr::Url("http://httpbin.org/put"));
+				cout << r.text;
+			}
+			else if (command == "patch") {
+				cpr::Response r = cpr::Patch(cpr::Url("http://httpbin.org/patch"));
+				cout << r.text;
+			}
+			else if (command == "post") {
+				cpr::Response r = cpr::Post(cpr::Url("http://httpbin.org/post"));
+				cout << r.text;
+			}
+			else if (command == "delete") {
+				cpr::Response r = cpr::Delete(cpr::Url("http://httpbin.org/delete"));
+				cout << r.text;
+			}
+		} while (command != "exit");
+	//case 2:
+		cpr::Response response = cpr::Get(cpr::Url("http://httpbin.org/headers"));// -accept: text / html"));
+		cpr::Header({ {"Accept",  "application / xhtml + xml, application / xml; q = 0.9, image / avif, image / webp, image / apng, */*;q=0.8,application/signed-exchange;v=b3;q=0.9"} });
+		cout << response.text;
+	//}
 	return 0;
 }
